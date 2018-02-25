47ec885fc0fa4d2eed8d19c67fc80dab332417a0
Makoto Tanabe <xekid78@outlook.com>
2018-02-25 16:32:12 +0900

    Add files via upload

---
+++ symbol.rb
@@ -0,0 +1,13 @@
+enemy1 = {"ザコ" => "スライム","中ボス" => "ドラゴン","ラスボス" => "魔王"}
+p enemy1
+p enemy1["ラスボス"]
+puts ""
+
+enemy2 = {:zako => "スライム",:boss => "ドラゴン",:king => "魔王"}
+p enemy2
+p enemy2[:king]
+puts ""
+
+enemy3 = {zako: "スライム",boss: "ドラゴン",king: "魔王"}
+p enemy3
+p enemy3[:king]
